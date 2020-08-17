442e27cf84f6ef108e830fdacf3d6301fe73eb5c
Ryan Kurz <ryankurz123@gmail.com>
2020-08-18 09:04:21 +1000

    to uni!

--- Assets/TrackedImageInfoManager.cs
+++ Assets/TrackedImageInfoManager.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARSubsystems;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.Networking;
 using System.Diagnostics;
-
+using UnityEngine.Networking.Match;
+using UnityEngine.Networking.NetworkSystem;
+using UnityEngine.Networking.Types;
 /// This component listens for images detected by the <c>XRImageTrackingSubsystem</c>
 /// and overlays some information as well as the source Texture2D on top of the
 /// detected image.
 /// </summary>
 [RequireComponent(typeof(ARTrackedImageManager))]
-public class TrackedImageInfoManager : MonoBehaviour
+public class TrackedImageInfoManager : NetworkBehaviour
 {
     [SerializeField]
     [Tooltip("The camera to set on the world space UI canvas for each instantiated image info.")]
@@ -32,7 +35,10 @@ public class TrackedImageInfoManager : MonoBehaviour
     private Touch touch;
     private Vector2 touchPosition;
 
+    [SyncVar]
     private Quaternion rotationY;
+
+    [SyncVar]
     private float deltaMagnitudeDiff = 0f;
 
     private Quaternion savedRotation;
@@ -74,8 +80,16 @@ public class TrackedImageInfoManager : MonoBehaviour
                 savedScale = trackedImage.transform.localScale;
                 deltaMagnitudeDiff = 0f;
             }
-
+            if (isServer)
+            {
+                UnityEngine.Debug.Log(rotationY);
+            }
+            else
+            {
+                UnityEngine.Debug.Log(rotationY);
+            }
             // Update rotation of Object
+
             trackedImage.transform.rotation = savedRotation * rotationY;
             savedRotation = trackedImage.transform.rotation;
             rotationY = Quaternion.Euler(0f, 0f, 0f);
@@ -85,14 +99,22 @@ public class TrackedImageInfoManager : MonoBehaviour
 
     void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
-        foreach (var trackedImage in eventArgs.added)
+
+        if (isLocalPlayer && isClient)
         {
-            // Give the initial image a reasonable default scale
-            trackedImage.transform.localScale = new Vector3(0.005f, 0.005f, 0.005f);
+
+            foreach (var trackedImage in eventArgs.added)
+            {
+                // Give the initial image a reasonable default scale
+                trackedImage.transform.localScale = new Vector3(0.005f, 0.005f, 0.005f);
+            }
+
+
+
+            foreach (var trackedImage in eventArgs.updated)
+                UpdateInfo(trackedImage);
         }
 
-        foreach (var trackedImage in eventArgs.updated)
-            UpdateInfo(trackedImage);
     }
 
 
@@ -103,54 +125,81 @@ public class TrackedImageInfoManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount == 1)
+        if(isLocalPlayer)
         {
-            Touch touch = Input.GetTouch(0);
-            switch (touch.phase)
+            if (isClient)
             {
-                //When a touch has first been detected, change the message and record the starting position
-                case TouchPhase.Began:
-                    startPos = touch.position;
-                    break;
 
-                case TouchPhase.Moved:
-                    // If we have moved we want to rotate
-                    rotationY = Quaternion.Euler(0f, -touch.deltaPosition.x * rotateSpeedModifier, 0f);
-                    break;
+                if (!isServer)
+                {
+                    UnityEngine.Debug.Log(rotationY);
+                }
 
-                case TouchPhase.Ended:
-                    // If when we ended the finger hadn't moved, it's a tap
-                    if (touch.position == startPos)
+                if (Input.touchCount == 1)
+                {
+                    Touch touch = Input.GetTouch(0);
+                    switch (touch.phase)
                     {
-                        Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-                        RaycastHit raycastHit;
-                        if (Physics.Raycast(raycast, out raycastHit))
-                        {
-                            if (raycastHit.collider != null)
+                        //When a touch has first been detected, change the message and record the starting position
+                        case TouchPhase.Began:
+                            startPos = touch.position;
+                            break;
+
+                        case TouchPhase.Moved:
+                            // If we have moved we want to rotate
+                            rotationY = Quaternion.Euler(0f, -touch.deltaPosition.x * rotateSpeedModifier, 0f);
+                            UnityEngine.Debug.Log(rotationY);
+                            break;
+
+                        case TouchPhase.Ended:
+                            // If when we ended the finger hadn't moved, it's a tap
+                            if (touch.position == startPos)
                             {
-                                UnityEngine.Debug.Log("Tapped " + raycastHit.transform.gameObject.name);
+                                Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                                RaycastHit raycastHit;
+                                if (Physics.Raycast(raycast, out raycastHit))
+                                {
+                                    if (raycastHit.collider != null)
+                                    {
+                                        UnityEngine.Debug.Log("Tapped " + raycastHit.transform.gameObject.name);
+                                    }
+                                }
                             }
-                        }
+                            break;
                     }
-                    break;
-            }  
-        } 
-        else if (Input.touchCount == 2)
-        {
-            // Store both of the touches on screen.
-            Touch touchZero = Input.GetTouch(0);
-            Touch touchOne = Input.GetTouch(1);
+                }
+                else if (Input.touchCount == 2)
+                {
+                    // Store both of the touches on screen.
+                    Touch touchZero = Input.GetTouch(0);
+                    Touch touchOne = Input.GetTouch(1);
 
-            // Find the position in the previous frame of each touch.
-            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+                    // Find the position in the previous frame of each touch.
+                    Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+                    Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
 
-            // Find the magnitude of the vector (the distance) between the touches in each frame.
-            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+                    // Find the magnitude of the vector (the distance) between the touches in each frame.
+                    float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+                    float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
 
-            // Find the difference in the distances between each frame.
-            deltaMagnitudeDiff = (prevTouchDeltaMag - touchDeltaMag) * -0.0001f;
+                    // Find the difference in the distances between each frame.
+                    deltaMagnitudeDiff = (prevTouchDeltaMag - touchDeltaMag) * -0.0001f;
+                }
+                if (!isServer) CmdUpdateRotation(rotationY);
+            }
+            
         }
+    }
+
+    [Command]
+    void CmdUpdateRotation(Quaternion rotation)
+    {
+        rotationY = rotation;
+    }
+
+    [Command]
+    void CmdUpdateScale(float deltaMagnitude)
+    {
+        deltaMagnitudeDiff = deltaMagnitude;
     }
 }
